25dc921e20d62e082b608655fc675d3ddcbfdbba
Zineb Barni <zinebbarni@gmail.com>
2023-06-18 00:10:26 +0100

    customer-data-service

---
+++ src/test/java/net/zineb/demo/CustomerDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.zineb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
